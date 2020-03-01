50734ecd232d9e35f214853334ce73dc61897f82
tobiasz18 <tobiaszpost@gmail.com>
2020-03-01 20:32:51 +0100

    api request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponentComponent } from './weather-component/weather-component.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpParams } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WeatherService {
+    url: string;
+
+    constructor(private httpClient: HttpClient) { }
+
+    public sendGETRequestWithParameters(lon, lat) {
+        let url = 'https://api.openweathermap.org/data/2.5/weather';
+        let keyApi = '148ab12495a9f1a901fe6056090a7487';
+
+        let params = new HttpParams();
+        params = params.append('lon', lon);
+        params = params.append('lat', lat);
+        params = params.append('appid', keyApi);
+
+        return this.httpClient.get(url, { params: params });
+    }
+
+}
